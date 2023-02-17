718f2b129bfb989240cff60a2344e07cc8c21baf
ISuckAtC <theporkchopsxd@gmail.com>
2023-02-17 16:05:46 +0100

    it works now

--- StraterraServer/StraterraServer.cpp
+++ StraterraServer/StraterraServer.cpp
@@ -1,169 +1,198 @@
 // StraterraServer.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
 #include <boost/asio.hpp>
-#include <thread>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <memory>
+#include <string>
+
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 
-using namespace boost::asio;
-using namespace boost::asio::ip;
+static int _code = 0;
 
-int readBytes(unsigned char* data, tcp::socket* socket, io_context* iocontext);
+class http_connection : public std::enable_shared_from_this<http_connection>
+{
+public:
+	http_connection(tcp::socket socket) : socket_(std::move(socket))
+	{
 
-void handleSocket(tcp::socket* socket, int threadId, io_context* iocontext);
+	}
 
-void handleRead(const boost::system::error_code& err, std::size_t);
+	void start()
+	{
+		read();
+		check_deadline();
+	}
+private:
+
+	// the underlying socket to read and write on network
+	tcp::socket socket_;
+
+	// the buffer to contain messages
+	beast::flat_buffer buffer_{ 1024 };
+
+	// the request message recieved from the client
+	http::request<http::dynamic_body> request_;
+
+	// the response message sent back to the client
+	http::response<http::dynamic_body> response_;
+
+	// the amount of time to wait before timing out
+	net::steady_timer deadline_{
+		socket_.get_executor(), std::chrono::seconds(20) };
+
+	// underlying read operation from client
+	void read()
+	{
+		// pointer to this object
+		auto self = shared_from_this();
+
+		http::async_read(
+			socket_,
+			buffer_,
+			request_,
+			[self](
+				beast::error_code ec,
+				std::size_t bytes_recieved)
+			{
+				boost::ignore_unused(bytes_recieved);
+		if (!ec) self->handle_request();
+			});
+	}
+
+	void handle_request()
+	{
+		// match the version of the request
+		response_.version(request_.version());
+
+		// handle and drop
+		response_.keep_alive(false);
+
+		// what http method is used
+		switch(request_.method())
+		{
+			// GET
+			case http::verb::get:
+				// response code
+				response_.result(http::status::ok);
+
+				// seems to be setting header
+				response_.set(http::field::server, "S_beast");
+
+
+				create_response();
+				break;
+		}
+
+		write_response();
+	}
+
+	void create_response()
+	{
+		// target location requested by client
+		std::string target = request_.target();
+		if (target == "/a")
+		{
+			// set content type (http is binary)
+			response_.set(http::field::content_type, "http");
+
+			// write content to stream
+			beast::ostream(response_.body())
+				<< _code++;
+		}
+		else
+		{
+			response_.set(http::field::content_type, "http");
+			beast::ostream(response_.body())
+				<< 0x01;
+		}
+	}
+
+	// async send response
+	void write_response()
+	{
+		// pointer to self
+		auto self = shared_from_this();
+
+		// set the amount of bytes being sent
+		response_.content_length(response_.body().size());
+
+		// write response to client
+		http::async_write(
+			socket_,
+			response_,
+			[self](beast::error_code ec, std::size_t bytes_sent)
+			{
+				// shut the socket down after use and cancel deadline
+				self->socket_.shutdown(tcp::socket::shutdown_send, ec);
+		self->deadline_.cancel();
+			}
+		);
+	}
+
+	void check_deadline()
+	{
+		auto self = shared_from_this();
+
+		// async_wait just waits equal to whats passed to the timer
+		deadline_.async_wait(
+			[self](beast::error_code ec)
+			{
+				if (!ec)
+				{
+					// close connection if deadline is passed
+					self->socket_.close(ec);
+				}
+			}
+		);
+	}
+};
+
+void http_server(tcp::acceptor& acceptor, tcp::socket& socket)
+{
+	std::cout << "Waiting for connection..." << std::endl;
+	acceptor.async_accept(socket,
+		[&](beast::error_code ec)
+		{
+			if (!ec)
+			{
+				
+				std::make_shared<http_connection>(std::move(socket))->start();
+				http_server(acceptor, socket);
+			}
+		});
+}
 
 int main()
 {
+	try
+	{
+		auto const address = net::ip::make_address_v4("0.0.0.0");
+		unsigned short port = 6969;
 
-    io_context iocontext;
-    int threadId = 0;
+		net::io_context ioc{ 1 };
 
-    try
-    {
-        tcp::endpoint endpoint(tcp::v4(), 6969);
+		tcp::acceptor acceptor{ ioc, {address, port} };
+		tcp::socket socket{ ioc };
 
-        std::cout << "START" << std::endl;
+		http_server(acceptor, socket);
 
-        tcp::acceptor acceptor(iocontext, endpoint);
-
-        std::cout << "ACCEPTOR" << std::endl;
-
-
-        for (;;)
-        {
-            tcp::socket socket(iocontext);
-
-            std::cout << "SOCKET" << std::endl;
-
-            acceptor.accept(socket);
-            
-            std::cout << "HANDLE: " << &socket << std::endl;
-
-            std::cout << "ACCEPT" << std::endl;
-
-            std::thread(handleSocket, &socket, threadId, &iocontext).detach();
-            threadId++;
-            
-        }
-    }
-    catch (const std::exception& ex)
-    {
-        std::cerr << ex.what() << std::endl;
-    }
+		ioc.run();
+		return 0;
+	}
+	catch (std::exception const& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
 }
-
-void handleRead(const boost::system::error_code& err, std::size_t)
-{
-
-}
-
-void handleSocket(tcp::socket* socket, int threadId, io_context* iocontext)
-{
-    try
-    {
-        std::cout << "[" << threadId << "]" << "HANDLING SOCKET" << std::endl;
-        unsigned char dataData[256];
-        int bytesRead;
-        char text[256];
-
-        std::cout << "[" << threadId << "]" << "HANDLE: " << socket << std::endl;
-
-        for (;;)
-        {
-            std::cout << "[" << threadId << "]" << "READING" << std::endl;
-            bytesRead = readBytes(dataData, socket, iocontext);
-
-            std::cout << "Bytes read: " << bytesRead << std::endl;
-
-            char* in = text;
-            unsigned char* out = dataData;
-
-            std::cout << "BEGIN MESSAGE: \"";
-
-            for (int i = 0; i < bytesRead; ++i)
-            {
-                std::cout << std::hex << (int)*out;
-                *in = (char)*out;
-                in++;
-                out++;
-            }
-            std::cout << "\"" << std::endl;
-
-            if (text == "end")
-            {
-                break;
-            }
-
-            *in = '\0';
-
-            std::cout << text << std::endl;
-        }
-    }
-    catch (const std::exception& ex)
-    {
-        std::cerr << "[" << threadId << "]" << ex.what() << std::endl;
-    }
-    (*socket).close();
-}
-
-int readBytes(unsigned char* data, tcp::socket* socket, io_context* iocontext)
-{
-    streambuf readBuf;
-    unsigned char* rBuf;
-    unsigned char* out;
-    int bRead = 0;
-
-    int* bReadP = &bRead;
-
-    unsigned char buf[256];
-
-    async_read(*socket, buffer(buf, 256), transfer_exactly(4), [bReadP](const boost::system::error_code& err, std::size_t size) {
-        std::cout << "Async got " << size << " bytes" << std::endl;
-        std::cout << "Read result: " << err << " - " << err.message() << std::endl;
-        *bReadP = size;
-        });
-
-    (*iocontext).run();
-
-    std::cout << "bRead: " << bRead << std::endl;
-
-    out = buf;
-
-    int size = (*(out) << 0 | *(out + 1) << 8 | *(out + 2) << 16 | *(out + 3) << 24);
-
-    std::cout << "SIZE: " << size << std::endl;
-
-    size -= 4;
-
-    async_read(*socket, buffer(buf, 256), transfer_exactly(size), [bReadP](const boost::system::error_code& err, std::size_t size) {
-        std::cout << "Async got " << size << " bytes" << std::endl;
-        *bReadP = size;
-        });
-
-    std::cout << "bRead: " << bRead << std::endl;
-
-    out = buf;
-
-    unsigned char* in = data;
-    
-    for (int i = 0; i < bRead; ++i)
-    {
-        *in++ = *out++;
-    }
-
-    return bRead;
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
