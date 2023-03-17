f9ebbbbe8a45cbe9dcd0d088c3e34b8bd938a6c7
ISuckAtC <theporkchopsxd@gmail.com>
2023-03-17 09:48:47 +0100

    a

--- StraterraServer/Http.cpp
+++ StraterraServer/Http.cpp
@@ -288,6 +288,7 @@ namespace Straterra
 					{
 						if (!ec)
 						{
+							std::cout << "+";
 							std::make_shared<http_connection>(std::move(socket))->start();
 							http_server(acceptor, socket);
 
